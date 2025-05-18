c25af24be6df2766b619441f12b8d0c049c950e6
Vivi Schulz <vivischulz@Vivis-MacBook-Pro.local>
2025-05-17 17:10:41 -0700

    Initial commit of divi child theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+// Enqueue parent and child styles
+function divi_child_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'divi_child_enqueue_styles');
+
+add_action('wp_enqueue_scripts', function() {
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;700&family=Open+Sans&display=swap');
+});
+
+
+add_action('wp_footer', function() {
+    echo '<p style="text-align:center; color:red;">Child theme is active!</p>';
+});
