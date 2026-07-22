c7aacfbdb13ee536ceaa5f200cf9a41c81f5bb29
Vivi Schulz <haweirh@gmail.com>
2026-07-21 17:43:29 -0700

    Update function.php add typography.css change color seconday primary

--- functions.php
+++ functions.php
@@ -22,3 +22,35 @@ add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400&family=Lora:wght@400;600&display=swap');
 });
 
+
+function vivi_enqueue_assets() {
+
+    // Base
+    wp_enqueue_style(
+        'typography',
+        get_stylesheet_directory_uri() . '/assets/css/base/typography.css'
+    );
+
+    wp_enqueue_style(
+        'global',
+        get_stylesheet_directory_uri() . '/assets/css/base/global.css'
+    ); // not used yet
+
+    // Components
+    wp_enqueue_style(
+        'hero',
+        get_stylesheet_directory_uri() . '/assets/css/components/hero.css'
+    ); // not used yet
+
+    wp_enqueue_style(
+        'buttons',
+        get_stylesheet_directory_uri() . '/assets/css/components/buttons.css'
+    ); // not used yet
+
+    wp_enqueue_style(
+        'faq',
+        get_stylesheet_directory_uri() . '/assets/css/components/faq.css'
+    ); // not used yet
+
+}
+add_action('wp_enqueue_scripts', 'vivi_enqueue_assets');
